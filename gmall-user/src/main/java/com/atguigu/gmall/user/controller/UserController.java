4cbbe257be9cb508dc912e49900d51c96025f6cb
zhufugao123 <zhufugao0822@sina.com>
2020-06-26 19:01:09 +0800

    day1

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(@Param("memberId") String memberId){
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddress = userService.getUmsMemberReceiveAddress(memberId);
+        return umsMemberReceiveAddress;
+    }
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers = userService.getAllUser();
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+}
