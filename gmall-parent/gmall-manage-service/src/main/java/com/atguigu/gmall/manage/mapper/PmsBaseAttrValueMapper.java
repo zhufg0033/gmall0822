7e36799224fe32e3bee494ef8863030487abad75
zhufugao123 <zhufugao0822@sina.com>
2020-06-27 18:37:18 +0800

    day2

---
+++ gmall-parent/gmall-manage-service/src/main/java/com/atguigu/gmall/manage/mapper/PmsBaseAttrValueMapper.java
@@ -0,0 +1,8 @@
+package com.atguigu.gmall.manage.mapper;
+
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
+import com.atguigu.gmall.bean.PmsBaseCatalog3;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PmsBaseAttrValueMapper extends Mapper<PmsBaseAttrValue> {
+}
